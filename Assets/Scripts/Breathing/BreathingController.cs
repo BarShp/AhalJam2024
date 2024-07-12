d704f842fec8920e8ce1020dad8794be1cda1124
BarShp <47710060+BarShp@users.noreply.github.com>
2024-07-12 22:39:34 +0300

    Added combast anxiety bar

--- Assets/Scripts/Breathing/BreathingController.cs
+++ Assets/Scripts/Breathing/BreathingController.cs
@@ -1,7 +1,9 @@
 using System;
 using Core;
 using PathCreation;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BreathingController : BaseMonoBehaviour
 {
@@ -10,17 +12,25 @@ public class BreathingController : BaseMonoBehaviour
     [SerializeField] private float thresholdBreathingOffset = 0.25f;
     [SerializeField] private float breathingPatternSpeed = 1f;
     [SerializeField] private float breathInOutSpeed = 0.25f;
+    [SerializeField] private float timeToMaxAnxietyInSeconds = 6;
+    [SerializeField] private float lowerAnxietySpeed = 0.125f;
 
     [Range(0.5f, 5f)]
     [SerializeField] private float breathingPointMaxLimitY;
     [Range(0.5f, 5f)]
     [SerializeField] private float breathingPointMinLimitY;
+
+    [SerializeField] private Image anxietyBar;
+    
+    private float anxietyGainSpeed;
     
     private VertexPath breathingPatternVertexPath;
     
     private float currentBreathingPointNormalized;
     private float targetBreathingPointNormalized;
     private float currentVelocity;
+
+    [ReadOnly] [SerializeField] private float currentAnxiety = 0;
 
     private float GetWorldLimitMinY() => transform.position.y - breathingPointMinLimitY;
     private float GetWorldLimitMaxY() => transform.position.y + breathingPointMaxLimitY;
@@ -29,6 +39,7 @@ public class BreathingController : BaseMonoBehaviour
     {
         breathingPatternVertexPath = breathingPatternPathCreator.path;
         currentBreathingPointNormalized = 0.5f;
+        anxietyGainSpeed = 1 / timeToMaxAnxietyInSeconds;
     }
 
     protected void Update()
@@ -74,12 +85,16 @@ public class BreathingController : BaseMonoBehaviour
 
         if (distanceFromPath > thresholdBreathingOffset)
         {
-            Debug.Log($"Too Far - ");
+            currentAnxiety += anxietyGainSpeed * Time.deltaTime;
         }
         else
         {
-            Debug.Log($"Close enough - ");
+            currentAnxiety -= lowerAnxietySpeed * Time.deltaTime;
         }
+
+        currentAnxiety = Mathf.Clamp(currentAnxiety, 0, 1);
+        
+        anxietyBar.fillAmount = currentAnxiety;
     }
 
     private void SetBreathingPointYPos(float normalizedValue)
