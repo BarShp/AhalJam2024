4c7537491bef9b87a5dc87e3ed80ee07a53d5960
Seanushka <Sean.nechaev@gmail.com>
2024-07-12 17:24:47 +0300

    *Player movement *Dialogue text *removed contact offset in physics2D settings

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    [SerializeField] DialogueSO textDialogue;
+    public TextMeshProUGUI textComponent;
+
+    private int index;
+
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialogue(textDialogue);
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == textDialogue.lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = textDialogue.lines[index];
+            }
+        }
+    }
+
+    void StartDialogue(DialogueSO newDialogueText)
+    {
+        index = 0;
+        textDialogue = newDialogueText;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach(char c in textDialogue.lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textDialogue.textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < textDialogue.lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine (TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+            
+    }
+}
